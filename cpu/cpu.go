847a9599b15c310645be8f5dfc7ecae11e92f865
Ross Gongaware <ross@gongaware.org>
2021-07-28 17:09:35 -0400

    Add convenience function for initializing registers
    This will be used when first setting up the system

--- cpu/cpu.go
+++ cpu/cpu.go
@@ -10,3 +10,12 @@ type Processor struct {
 
 	memory *memory.CPUMap
 }
+
+func (proc *Processor) initializeRegisters() {
+	//SP initally points to the first free byte on the first page.
+	//SP is decrmented so this would be 0xFF (0x01FF)
+	proc.registers[StackPointer] = 0xFF
+
+	//Bit 5 is not used and always 1
+	proc.registers[Status] = 0b0010_0000
+}
