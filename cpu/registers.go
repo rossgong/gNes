e5b69e69aecb8d7c32507d036f67536a7d68cd6c
Ross Gongaware <ross@gongaware.org>
2021-07-28 18:04:18 -0400

    Add doc comment explaining the status register

--- cpu/registers.go
+++ cpu/registers.go
@@ -6,6 +6,27 @@ const (
 	X
 	Y
 	StackPointer
+	/*
+		Status holds various flags set by instructions.
+
+		Bit	Meaning
+
+		0	Carry (1 means carry)
+
+		1	Zero (1 means zero)
+
+		2	IRQ (1 means disable)
+
+		3	Decimal Mode (1 means on)
+
+		4	Break (1 means Reset)
+
+		5	UNUSED (ALWAYS 1)
+
+		6	Overflow (1 means overflow)
+
+		7	Negative (1 means negative)
+	*/
 	Status
 	//Number of normal CPU registers (excludes PC)
 	numRegisters
