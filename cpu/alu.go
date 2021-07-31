b85526c75c549ffd8e8240a4ef6c16a9467b7173
Ross Gongaware <ross@gongaware.org>
2021-07-31 12:40:06 -0400

    Refactor files and split up instructions by category

--- cpu/alu.go
+++ cpu/alu.go
@@ -1,7 +1,5 @@
 package cpu
 
-import "gongaware.org/gNES/memory"
-
 /*
 ADC
 
@@ -56,33 +54,6 @@ func (proc *Processor) ShiftLeft(operand byte) byte {
 }
 
 /*
-BCC
-
-Branch if no carry (CarryFlag = 0)
-*/
-func (proc *Processor) BranchOnCarryClear(address memory.Address) {
-	proc.branchOnFlagClear(address, CarryFlag)
-}
-
-/*
-BCS
-
-Branch if there is carry (CarryFlag = 1)
-*/
-func (proc *Processor) BranchOnCarrySet(address memory.Address) {
-	proc.branchOnFlagSet(address, CarryFlag)
-}
-
-/*
-BEQ
-
-Branch if result is zero (ZeroFlag = 1)
-*/
-func (proc *Processor) BranchOnEqual(address memory.Address) {
-	proc.branchOnFlagSet(address, ZeroFlag)
-}
-
-/*
 BIT Test bits
 Bit 6+7 (Negative and overflow) are transferred
 The zero flag is then set according to A&operand
@@ -97,54 +68,7 @@ func (proc *Processor) BitTest(operand byte) {
 	proc.setZeroFlag(proc.registers[A] & operand)
 }
 
-/*
-BMI
-Branch on Minus (NegativeFlag = 1)
-*/
-func (proc *Processor) BranchOnNegative(address memory.Address) {
-	proc.branchOnFlagSet(address, NegativeFlag)
-}
-
-/*
-BNE
-
-Branch if not Zero (ZeroFlag = 0)
-*/
-func (proc *Processor) BranchOnNotEqual(address memory.Address) {
-	proc.branchOnFlagClear(address, ZeroFlag)
-}
-
-/*
-BPL
-
-Branch if plus (NegativeFlag = 0)
-*/
-func (proc *Processor) BranchOnPlus(address memory.Address) {
-	proc.branchOnFlagClear(address, NegativeFlag)
-}
-
-/*
-BRK
-
-Software interrupt.
-
-Push PC+2 to the stack (instruction + interrupt mark). Set break status to 1 and then push to stack
-*/
-// func (proc *Processor)
-
-//Utility functions
-func (proc *Processor) branchOnFlagSet(address memory.Address, flag byte) {
-	if proc.registers[Status]&flag == flag {
-		proc.pc = address
-	}
-}
-
-func (proc *Processor) branchOnFlagClear(address memory.Address, flag byte) {
-	if proc.registers[Status]&flag == 0 {
-		proc.pc = address
-	}
-}
-
+//Utility Functions
 func (proc *Processor) setZeroFlag(result byte) {
 	if result == 0 {
 		proc.setStatusFlags(ZeroFlag)
---
+++ cpu/branch.go
@@ -0,0 +1,69 @@
+package cpu
+
+import "gongaware.org/gNES/memory"
+
+/*
+BCC
+
+Branch if no carry (CarryFlag = 0)
+*/
+func (proc *Processor) BranchOnCarryClear(address memory.Address) {
+	proc.branchOnFlagClear(address, CarryFlag)
+}
+
+/*
+BCS
+
+Branch if there is carry (CarryFlag = 1)
+*/
+func (proc *Processor) BranchOnCarrySet(address memory.Address) {
+	proc.branchOnFlagSet(address, CarryFlag)
+}
+
+/*
+BEQ
+
+Branch if result is zero (ZeroFlag = 1)
+*/
+func (proc *Processor) BranchOnEqual(address memory.Address) {
+	proc.branchOnFlagSet(address, ZeroFlag)
+}
+
+/*
+BMI
+Branch on Minus (NegativeFlag = 1)
+*/
+func (proc *Processor) BranchOnNegative(address memory.Address) {
+	proc.branchOnFlagSet(address, NegativeFlag)
+}
+
+/*
+BNE
+
+Branch if not Zero (ZeroFlag = 0)
+*/
+func (proc *Processor) BranchOnNotEqual(address memory.Address) {
+	proc.branchOnFlagClear(address, ZeroFlag)
+}
+
+/*
+BPL
+
+Branch if plus (NegativeFlag = 0)
+*/
+func (proc *Processor) BranchOnPlus(address memory.Address) {
+	proc.branchOnFlagClear(address, NegativeFlag)
+}
+
+//Utility functions
+func (proc *Processor) branchOnFlagSet(address memory.Address, flag byte) {
+	if proc.registers[Status]&flag == flag {
+		proc.pc = address
+	}
+}
+
+func (proc *Processor) branchOnFlagClear(address memory.Address, flag byte) {
+	if proc.registers[Status]&flag == 0 {
+		proc.pc = address
+	}
+}
